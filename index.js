ff360abd32acc49bce0a0a9bde492f1be4c4bc12
GabrielSzalona <82843739+GabrielSzalona@users.noreply.github.com>
2024-02-18 14:04:53 +0100

    modified

--- index.js
+++ index.js
@@ -29,9 +29,6 @@ async function getPeopleInGym() {
 }
 
 
-const inGym = await getPeopleInGym()
-console.log(`people in gym ${JSON.stringify(inGym)}`)
-
 
 // sources
 // https://docs.gymify.app/wp-content/uploads/2022/05/gymify_logo.png
@@ -46,7 +43,7 @@ if (param != null && param.length > 0) {
 
 const widget = new ListWidget()
 const storeInfo = await fetchStoreInformation()
-const storeCapacity = await fetchAmountOfPaper()
+const storeCapacity = await getPeopleInGym()
 await createWidget()
 
 // used for debugging if script runs inside the app
@@ -60,7 +57,7 @@ Script.complete()
 async function createWidget() {
 
     widget.addSpacer(4)
-    const logoImg = await getImage('dm-logo.png')
+    const logoImg = await getImage('gymify.png')
 
     widget.setPadding(10, 10, 10, 10)
     const titleFontSize = 12
@@ -73,114 +70,65 @@ async function createWidget() {
     logoImageStack.backgroundColor = new Color("#ffffff", 1.0)
     logoImageStack.cornerRadius = 8
     const wimg = logoImageStack.addImage(logoImg)
-    wimg.imageSize = new Size(40, 40)
+    wimg.imageSize = new Size(125, 40)
     wimg.rightAlignImage()
     widget.addSpacer()
 
-    const icon = await getImage('toilet-paper.png')
+    // const icon = await getImage('toilet-paper.png')
     let row = widget.addStack()
     row.layoutHorizontally()
-    row.addSpacer(2)
-    const iconImg = row.addImage(icon)
-    iconImg.imageSize = new Size(40, 40)
-    row.addSpacer(13)
+    // row.addSpacer(2)
+    // const iconImg = row.addImage(icon)
+    // iconImg.imageSize = new Size(40, 40)
+    // row.addSpacer(13)
 
     let column = row.addStack()
     column.layoutVertically()
 
-    const paperText = column.addText("KLOPAPIER")
+    const paperText = column.addText("PEOPLE IN GYM")
     paperText.font = Font.mediumRoundedSystemFont(13)
 
     const packageCount = column.addText(storeCapacity.toString())
     packageCount.font = Font.mediumRoundedSystemFont(22)
-    if (storeCapacity < 30) {
-        packageCount.textColor = new Color("#E50000")
-    } else {
+    if (storeCapacity < 5) {
         packageCount.textColor = new Color("#00CD66")
+    } else {
+        packageCount.textColor = new Color("#E50000")
     }
     widget.addSpacer(4)
 
     const row2 = widget.addStack()
     row2.layoutVertically()
 
-    const street = row2.addText(storeInfo.address.street)
-    street.font = Font.regularSystemFont(11)
+    // const street = row2.addText(storeInfo.address.street)
+    // street.font = Font.regularSystemFont(11)
 
-    const zipCity = row2.addText(storeInfo.address.zip + " " + storeInfo.address.city)
-    zipCity.font = Font.regularSystemFont(11)
+    // const zipCity = row2.addText(storeInfo.address.zip + " " + storeInfo.address.city)
+    // zipCity.font = Font.regularSystemFont(11)
 
-    let currentTime = new Date().toLocaleTimeString('de-DE', { hour: "numeric", minute: "numeric" })
-    let currentDay = new Date().getDay()
-    let isOpen
-    if (currentDay > 0) {
-        const todaysOpeningHour = storeInfo.openingHours[currentDay - 1].timeRanges[0].opening
-        const todaysClosingHour = storeInfo.openingHours[currentDay - 1].timeRanges[0].closing
-        const range = [todaysOpeningHour, todaysClosingHour];
-        isOpen = isInRange(currentTime, range)
-    } else {
-        isOpen = false
-    }
+    // let currentTime = new Date().toLocaleTimeString('de-DE', { hour: "numeric", minute: "numeric" })
+    // let currentDay = new Date().getDay()
+    // let isOpen
+    // if (currentDay > 0) {
+    //     const todaysOpeningHour = storeInfo.openingHours[currentDay - 1].timeRanges[0].opening
+    //     const todaysClosingHour = storeInfo.openingHours[currentDay - 1].timeRanges[0].closing
+    //     const range = [todaysOpeningHour, todaysClosingHour];
+    //     isOpen = isInRange(currentTime, range)
+    // } else {
+    //     isOpen = false
+    // }
 
-    let shopStateText
-    if (isOpen) {
-        shopStateText = row2.addText('Geöffnet')
-        shopStateText.textColor = new Color("#00CD66")
-    } else {
-        shopStateText = row2.addText('Geschlossen')
-        shopStateText.textColor = new Color("#E50000")
-    }
-    shopStateText.font = Font.mediumSystemFont(11)
+    // let shopStateText
+    // if (isOpen) {
+    //     shopStateText = row2.addText('Geöffnet')
+    //     shopStateText.textColor = new Color("#00CD66")
+    // } else {
+    //     shopStateText = row2.addText('Geschlossen')
+    //     shopStateText.textColor = new Color("#E50000")
+    // }
+    // shopStateText.font = Font.mediumSystemFont(11)
 }
 
-// fetches the amount of toilet paper packages
-async function fetchAmountOfPaper() {
-    let url
-    let counter = 0
-    if (country.toLowerCase() === 'at') {
-        // Austria
-        const array = ["156754", "180487", "194066", "188494", "194144", "273259", "170237", "232201", "170425", "283216", "205873", "205874", "249881", "184204"]
-        for (var i = 0; i < array.length; i++) {
-            let currentItem = array[i]
-            url = 'https://products.dm.de/store-availability/AT/products/dans/' + currentItem + '/stocklevel?storeNumbers=' + storeId
-            let req = new Request(url)
-            let apiResult = await req.loadJSON()
-            if (req.response.statusCode == 200) {
-                counter += apiResult.storeAvailability[0].stockLevel
-            }
-        }
-    } else {
-        // Germany
-        url = 'https://products.dm.de/store-availability/DE/availability?dans=595420,708997,137425,28171,485698,799358,863567,452740,610544,846857,709006,452753,879536,452744,485695,853483,594080,504606,593761,525943,842480,535981,127048,524535&storeNumbers=' + storeId
-        const req = new Request(url)
-        const apiResult = await req.loadJSON()
-        for (var i in apiResult.storeAvailabilities) {
-            if (apiResult.storeAvailabilities[i][0].stockLevel) {
-                counter += apiResult.storeAvailabilities[i][0].stockLevel
-            }
-        }
-    }
-    return counter
-}
-
-// fetches information of the configured store, e.g. opening hours, address etc.
-async function fetchStoreInformation() {
-    let url
-    if (country.toLowerCase() === 'at') {
-        url = 'https://store-data-service.services.dmtech.com/stores/item/at/' + storeId
-        widget.url = 'https://www.dm.at/search?query=toilettenpapier&searchType=product'
-    } else {
-        url = 'https://store-data-service.services.dmtech.com/stores/item/de/' + storeId
-        widget.url = 'https://www.dm.de/search?query=toilettenpapier&searchType=product'
-    }
-    let req = new Request(url)
-    let apiResult = await req.loadJSON()
-    return apiResult
-}
-
-// checks whether the store is currently open or closed
-function isInRange(value, range) {
-    return value >= range[0] && value <= range[1];
-}
 
 // get images from local filestore or download them once
 async function getImage(image) {
@@ -193,7 +141,7 @@ async function getImage(image) {
         // download once
         let imageUrl
         switch (image) {
-            case 'dm-logo.png':
+            case 'gymify.png':
                 imageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/Dm_Logo.svg/300px-Dm_Logo.svg.png"
                 break
             case 'toilet-paper.png':
